f6ecb9cc2f021147fa34c59a764462d4427fe4db
Damon <Damon>
2017-02-01 19:02:50 -0500

    done exercise 3 - Damon

--- Controllers/CustomerCareController.cs
+++ Controllers/CustomerCareController.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+
+[Route("customer")]
 public class CustomerCareController : Controller
 {
     public IActionResult Index()
     {
     return Ok("This is the index of the CustomerCareController");
     }
-    public IActionResult Claims ()
+        
+    [Route("claim/fileclaim")]
+    public IActionResult NewClaim()
     {
-    return Ok("This is the claims of the CustomerCareController");
+    return Ok("Newclaim of the CustomerCareController");
+    }
+
+    [Route("claim/myclaims")]
+    public IActionResult ClaimHistory()
+    {
+    return Ok("ClaimHistory of the CustomerCareController");
     }
 }
